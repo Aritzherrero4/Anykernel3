3d46016d8e9b930db892ab3b26dd9b45ff741876
Aritz Herrero <aritzherrero4@gmail.com>
2019-11-07 21:33:41 +0100

    Bringup for Amethyst Kernel 	Android 9 and OxygenOS only

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Amethyst kernel by aritzherrero4 
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+do.cleanuponabort=1
+device.name1=OnePlus5T
+device.name2=dumpling
+device.name3=OnePlus5
+device.name4=cheeseburger
 device.name5=
-supported.versions=
+supported.versions=9.0
 supported.patchlevels=
 '; } # end properties
 
@@ -34,27 +34,29 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+#check if user is in OxygenOS
+userflavor="$(file_getprop /system/build.prop "ro.build.user"):$(file_getprop /system/build.prop "ro.build.flavor")";
+
+case "$userflavor" in
+  "OnePlus:OnePlus5-user"|"OnePlus:OnePlus5T-user")
+    ui_print " ";
+    ui_print "You are on OxygenOS!";;
+   *)
+    ui_print " ";
+    ui_print "You are not on OxygenOS! Aborting install...";
+    exit 1;;
+esac;
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
+android_version="$(file_getprop /system/build.prop "ro.build.version.release")";
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+if [ -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+  patch_cmdline "skip_override" "skip_override";
+fi;
 
 # end ramdisk changes
 
